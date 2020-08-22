e220fe9dbd33e442db62bc9af94270624a6200e8
zdxdsw <36400315+zdxdsw@users.noreply.github.com>
2020-08-22 09:08:16 +0800

    Add COMP4021

--- world-travel/continue_status.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-// The following line of code reads the file containing all 
-// the chatroom messages and 'prints' it out to the browser
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: text/plain; charset=utf-8');
-readfile('status.txt');
-?> 
